aa44ed58af62f1703275372d53f424fc9a6093d7
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-12-26 07:56:26 +0100

    done with layout stuffs

--- lib/main.dart
+++ lib/main.dart
@@ -9,46 +9,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> names = ['Dara', 'Dara', 'dara'];
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          backgroundColor: Colors.deepPurple[200],
-          appBar: AppBar(
-            title: Center(
-                child: Text(
-              'My App',
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            )),
-            backgroundColor: Colors.deepPurple,
-            elevation: 0,
-            leading: Icon(
-              Icons.menu,
-              color: Colors.white,
-            ),
-            actions: [
-              IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.logout,
-                    color: Colors.white,
-                  )),
-            ],
-          ),
-          body: Center(
-            child: Container(
-              height: 300,
-              width: 300,
-              decoration: BoxDecoration(
-                  color: Colors.deepPurple,
-                  borderRadius: BorderRadius.circular(20)),
-              child: Center(
-                  child: Icon(
-                Icons.favorite,
-                color: Colors.white,
-                size: 30,
-              )),
-            ),
-          )),
+        body: ListView.builder(
+          itemCount: names.length, // Use the length of the list
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(names[index]), // Pass the correct `index`
+            );
+          },
+        ),
+      ),
     );
   }
 }
