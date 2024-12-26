9827a9a6ef3056b9b267a9cc461ca6fd68cde498
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-12-26 10:02:11 +0100

    done with basic routing moving to drawers

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -7,22 +7,35 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text(
-              'First Page',
-              style: TextStyle(color: Colors.white),
-            ),
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'First Page',
+            style: TextStyle(color: Colors.white),
           ),
-          backgroundColor: Colors.deepPurple,
         ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/secondpage');
-            },
-            child: Text('Go to Second Page'),
-          ),
-        ));
+        backgroundColor: Colors.deepPurple,
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.deepPurple[200],
+        child: Column(
+          children: [
+            DrawerHeader(
+              child: Icon(Icons.favorite),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('HOME'),
+              onTap: () => {},
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('SETTINGS'),
+              onTap: () => {},
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
