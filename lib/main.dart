79805df29f459ae91dbf875621f01c7191ec9d5a
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2025-01-02 16:33:23 +0100

    done learning providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/pages/count_model.dart';
 import 'package:flutter_application_2/pages/count_page.dart';
+import 'package:provider/provider.dart';
 
-void main() async {
-  runApp(MyApp());
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => CountModel(),
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,6 +15,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CountPage();
+    return MaterialApp(
+      title: 'Count app',
+      debugShowCheckedModeBanner: false,
+      home: const CountPage(), // <--- Now inside MaterialApp
+    );
   }
 }
--- lib/pages/count_page.dart
+++ lib/pages/count_page.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/pages/count_model.dart';
+import 'package:provider/provider.dart';
 
 class CountPage extends StatelessWidget {
   const CountPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple[300],
-        title: Center(child: Text('Flutter Demo Home Page')),
-      ),
-      body: Column(
-        children: [
-          Text(
-            'You have pushed the button this many times',
-            style: TextStyle(),
-          ),
-          Text(
-            '0',
-            style: TextStyle(),
-          ),
-        ],
-      ),
-    );
+    return Consumer<CountModel>(
+        builder: (context, value, index) => Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.deepPurple[300],
+                title: const Center(child: Text('Flutter Demo Home Page')),
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                  final counter = context.read<CountModel>();
+                  counter.increment();
+                },
+                child: const Icon(Icons.add),
+              ),
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'You have pushed the button this many times',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    Text(
+                      value.count.toString(),
+                      style: const TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ));
   }
 }
